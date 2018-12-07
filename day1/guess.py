ce096572fb6609cc6d84d602a46ec38644bebfc4
keyjia <keyjia@idream-china.com>
2018-12-07 12:20:54 +0800

    commint

--- day1/guess.py
+++ day1/guess.py
@@ -26,21 +26,6 @@ while True:
 
 
 
-age = 32
-count = 0
-while count <3:
-    guess_age = int(input("guess age:"))
-    if guess_age == age:
-        print("Yes you got it. ")
-        break
-    elif guess_age> age:
-        print("think smaller...")
-    else:
-        print("think bigger!")
-    count +=1
-
-
-
 
 
 
